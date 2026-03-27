60d90453ab9c47875030e85f8f84d05e0cd6de3a
umapathy1729 <umapathy428@gmail.com>
2026-03-27 12:11:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,32 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from EKS!');
+  res.send(`
+    <html>
+      <head>
+        <title>My EKS App</title>
+        <style>
+          body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+            background-color: #f0f8ff; /* light background */
+          }
+          h1 {
+            font-size: 72px;        /* big font */
+            color: #ff4500;         /* orange-red color */
+            text-align: center;
+            font-family: Arial, sans-serif;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Hello from EKS!</h1>
+      </body>
+    </html>
+  `);
 });
 
 app.listen(PORT, () => {
